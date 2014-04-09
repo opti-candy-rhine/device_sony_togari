9873359e44c00ef93636bce4366f15d591a60617
Josue Rivera <prbassplayer@gmail.com>
2014-04-09 04:42:21 +0200

    Slim basics
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

---
+++ slim.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit Slim common Phone stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/togari/full_togari.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_togari
+PRODUCT_DEVICE := togari
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
