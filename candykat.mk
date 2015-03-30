889a3183ca4b11a7c98b8db428deb07d398d00e6
Saatvik Shukla <saatvik@obiwan.gothdroid.com>
2015-03-30 09:58:12 +0200

    Candykat bringup

--- candykat.mk
+++ candykat.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/candykat/config/gsm.mk)
 
 # Inherit Slim common Phone stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/candykat/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candykat/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/togari/full_togari.mk)
@@ -19,7 +19,7 @@ PRODUCT_RELEASE_NAME := togari
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := togari
-PRODUCT_NAME := slim_togari
+PRODUCT_NAME := candykat_togari
 PRODUCT_BRAND := Xperia
 PRODUCT_MODEL := Z Ultra
 PRODUCT_MANUFACTURER := Sony
